b18a7347174b1a2245fe554606d6e67321e007a5
mingzhi.xie <xie6032mail@163.com>
2019-04-22 14:25:51 +0800

    Add hasInterface

--- src/main/java/com/miven/collection/list/ArrayListExample.java
+++ src/main/java/com/miven/collection/list/ArrayListExample.java
@@ -6,5 +6,23 @@ package com.miven.collection.list;
  * @author mingzhi.xie
  * @date 2019/4/21
  */
-public class ArrayListExample {
+class ArrayListExample {
+
+    /**
+     * 是否有显示实现指定接口
+     * @param t Object
+     * @param clazz clazz
+     * @param <T> T
+     * @return true:有，false:没有
+     */
+    <T> boolean hasInterface(T t, Class clazz) {
+        Class<?>[] interfaces = t.getClass().getInterfaces();
+        for (Class<?> c : interfaces) {
+            if (c.equals(clazz)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
