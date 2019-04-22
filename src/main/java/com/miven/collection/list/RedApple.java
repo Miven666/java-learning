4c0c893ffe191527678aa7d8fca23faa6d7a98b6
mingzhi.xie <xie6032mail@163.com>
2019-04-22 14:27:02 +0800

    Add class and interface example

---
+++ src/main/java/com/miven/collection/list/RedApple.java
@@ -0,0 +1,15 @@
+package com.miven.collection.list;
+
+/**
+ * 红苹果
+ *
+ * @author mingzhi.xie
+ * @date 2019/4/22.
+ */
+public class RedApple extends Apple implements Call {
+
+    @Override
+    public String name() {
+        return "I'm name is RedApple";
+    }
+}
