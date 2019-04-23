a4340fb4de8d5c4c51be49f50fcf03d74fea1e38
mingzhi.xie <xie6032mail@163.com>
2019-04-23 09:33:30 +0800

    Add array example test

---
+++ src/test/java/com/miven/array/ArrayExampleTest.java
@@ -0,0 +1,44 @@
+package com.miven.array;
+
+import org.junit.Test;
+
+/**
+ * 数组 测试类
+ */
+public class ArrayExampleTest {
+
+    private int[] defaultBaseArr = {1, 2, 3, 4, 5};
+    private Person[] defaultRefArr = {
+            new Person("11"),
+            new Person("22"),
+            new Person("33"),
+            new Person("44"),
+            new Person("55")
+    };
+
+    private ArrayExample example = new ArrayExample();
+
+    /**
+     * copyOf() 测试
+     */
+    @Test
+    public void copyOfTest() {
+        example.baseTypeCopy(defaultBaseArr);
+        System.out.println();
+        System.out.println("---------分割符----------");
+        System.out.println();
+        example.referenceTypeCopy(defaultRefArr);
+    }
+
+    /**
+     * clone() 测试
+     */
+    @Test
+    public void cloneTest() {
+        example.baseTypeClone(defaultBaseArr);
+        System.out.println();
+        System.out.println("---------分割符----------");
+        System.out.println();
+        example.referenceTypeClone(defaultRefArr);
+    }
+}
