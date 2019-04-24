88d7581322bb4e04ae803301a72b6279d9d9095a
mingzhi.xie <xie6032mail@163.com>
2019-04-24 16:11:20 +0800

    Add ThreadPool test

---
+++ src/test/java/com/miven/pool/ThreadPoolExampleTest.java
@@ -0,0 +1,16 @@
+package com.miven.pool;
+
+import org.junit.Test;
+
+public class ThreadPoolExampleTest {
+
+    @Test
+    public void ctlTest() {
+        System.out.println(-1 << 29);
+        System.out.println(0);
+        System.out.println(1 << 29);
+        System.out.println(2 << 29);
+        System.out.println(3 << 29);
+    }
+
+}
