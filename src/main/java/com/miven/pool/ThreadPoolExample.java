8de5aeaf346f5ab08cfc91550ae691a31dd1aadd
mingzhi.xie <xie6032mail@163.com>
2019-04-25 13:39:46 +0800

    Add create threadPoolExecutor method

--- src/main/java/com/miven/pool/ThreadPoolExample.java
+++ src/main/java/com/miven/pool/ThreadPoolExample.java
@@ -1,10 +1,25 @@
 package com.miven.pool;
 
+import java.util.concurrent.*;
+
 /**
  * 线程池 例子
  *
  * @author mingzhi.xie
  * @date 2019/4/23.
  */
-public class ThreadPoolExample {
+class ThreadPoolExample {
+
+    ThreadPoolExecutor createThreadPoolExecutor() {
+        // init params
+        int corePoolSize = 5;
+        int maximumPoolSize = 15;
+        long keepAliveTime = 20;
+        BlockingQueue<Runnable> workQueue = new LinkedBlockingQueue<>();
+        ThreadFactory threadFactory = Executors.defaultThreadFactory();
+        RejectedExecutionHandler handler = new ThreadPoolExecutor.AbortPolicy();
+
+        return new ThreadPoolExecutor(
+                corePoolSize, maximumPoolSize, keepAliveTime, TimeUnit.SECONDS, workQueue, threadFactory, handler);
+    }
 }
