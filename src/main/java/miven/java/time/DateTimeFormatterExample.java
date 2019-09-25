fb1b88327081507441ac25bf5d2c2c4fef2ce0ac
mingzhi.xie <xie6032mail@163.com>
2019-09-25 11:33:26 +0800

    Add DataTimeFormatter Test

--- src/main/java/miven/java/time/DateTimeFormatterExample.java
+++ src/main/java/miven/java/time/DateTimeFormatterExample.java
@@ -7,26 +7,28 @@ import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 /**
+ * DateTimeFormatter 例子
  * @author mingzhi.xie
  * @date 2019/6/3.
+ * @see DateTimeFormatter
  */
-public class DateTimeFormatterExample {
+class DateTimeFormatterExample {
 
-    public static void main(String[] args) {
-
+    static void formatTime() {
         String defaultZoneId = ZoneId.systemDefault().toString();
-        System.out.println("defaultZoneId: " + defaultZoneId);
+        System.out.println("默认时区 defaultZoneId: " + defaultZoneId);
 
         String localDateTime = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(LocalDateTime.now());
-        System.out.println("localDateTime: " + localDateTime);
+        System.out.println("本地时间 localDateTime: " + localDateTime);
 
         String zonedDateTime = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(ZonedDateTime.now());
-        System.out.println("zonedDateTime: " + zonedDateTime);
-
-        String timestamp = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(new Timestamp(System.currentTimeMillis()).toLocalDateTime());
-        System.out.println("timestamp: " + timestamp);
+        System.out.println("时区时间 zonedDateTime: " + zonedDateTime);
 
         String isoDateTime = DateTimeFormatter.ISO_DATE_TIME.format(LocalDateTime.now());
-        System.out.println("isoDateTime: " + isoDateTime);
+        System.out.println("标准时间 isoDateTime: " + isoDateTime);
+
+        long millis = System.currentTimeMillis();
+        System.out.println("当前时间毫秒值 currentTimeMillis: " + millis);
+        System.out.println("当前时间时间戳 timestamp: " + new Timestamp(millis));
     }
 }
