046c40e237b47535a0d25bfe2dadc41e5b369e36
mingzhi.xie <xie6032mail@163.com>
2019-09-25 11:13:56 +0800

    Add Map

--- src/main/java/miven/java/collection/Collection.md
+++ src/main/java/miven/java/collection/Collection.md
@@ -3,3 +3,5 @@
 ## [ArrayList](list/ArrayList.md)
 
 ## [Hashtable](map/Hashtable.md)
+
+## [Map](map/Map.md)
---
+++ src/test/java/miven/java/collection/map/MapExampleTest.java
@@ -0,0 +1,13 @@
+package miven.java.collection.map;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MapExampleTest {
+
+    @Test
+    public void putAndPutIfAbsent() {
+        MapExample.putAndPutIfAbsent();
+    }
+}
