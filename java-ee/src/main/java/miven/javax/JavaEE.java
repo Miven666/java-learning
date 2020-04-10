01dd8f71ac66a0849bbc1eb0e74e7fdae0a588ac
mingzhi.xie <xie6032mail@163.com>
2020-04-11 02:22:55 +0800

    调整Jar包依赖

---
+++ java-ee/src/main/java/miven/javax/JavaEE.java
@@ -0,0 +1,13 @@
+package miven.javax;
+
+/**
+ * @author mingzhi.xie
+ * @date 2019/8/12
+ * @since 1.0
+ */
+public class JavaEE {
+
+    public static void main(String[] args) {
+        System.out.println("Hello JavaEE!");
+    }
+}
---
+++ java-maven/src/main/java/miven/maven/MavenExample.java
@@ -0,0 +1,17 @@
+package miven.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mingzhi.xie
+ * @date 2020/4/10
+ * @since 1.0
+ */
+@SpringBootApplication
+public class MavenExample {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenExample.class, args);
+    }
+}
