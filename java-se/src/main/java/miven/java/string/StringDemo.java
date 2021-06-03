0f8b7f846c8901df47997d535ae9c0a9385507ec
mingzhi.xie <xie6032mail@163.com>
2021-06-03 13:53:03 +0800

    添加 String hashCode 示例

--- java-se/src/main/java/miven/java/string/StringDemo.java
+++ java-se/src/main/java/miven/java/string/StringDemo.java
@@ -1,5 +1,7 @@
 package miven.java.string;
 
+import java.util.Arrays;
+
 /**
  * {@link String} 示例
  *
@@ -9,12 +11,19 @@ package miven.java.string;
 public class StringDemo {
 
     public static void main(String[] args) {
-        stringByteLength();
+        printByteLength();
+        printHashCode();
+        printHashCode();
     }
 
-    private static void stringByteLength() {
+    private static void printByteLength() {
         System.out.println("测".getBytes().length);
         System.out.println("TB".getBytes().length);
         System.out.println("TB\nAA".replace("\n", ""));
+        System.out.println(Arrays.toString("|test".split("\\|")));
+    }
+
+    private static void printHashCode() {
+        System.out.println("test#demo".hashCode());
     }
 }
